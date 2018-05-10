47372a7b81aea3aa10a9f6a76e629b9102b76a44
Max Kaye <m@xk.io>
2018-05-10 11:42:21 +1000

    Update deploy for better / easier deployments

--- bin/deploy.js
+++ bin/deploy.js
@@ -34,110 +34,62 @@ const ethHashCheck = (h, lenBytes = 32) => {
 
 
 const main = async () => {
-    const args_ = yargs.options({
-        "contractName": {
+    const args = yargs.options({
+        "scName": {
             type: 'string',
             describe: "The contract name of an alternate contract to deploy, do not include '.sol'",
             demand: true
         },
-        "contractDir": {
+        "scSrc": {
             type: 'string',
             describe: 'contract build dir to use (expects ./CONTRACT_DIR/CONTRACT_NAME.bin to exist)',
+            default: "_solDist",
             demand: false,
         },
-        "web3Provider": {
+        "web3": {
             describe: "URI for web3 provider - HTTP only",
             default: "https://kovan.eth.secure.vote:8545",
             type: 'string'
         },
         "argsJson": {
             type: 'string',
-            describe: "Args to provide with contractName in json encoding - required for deploying arbitrary",
+            describe: "Args to provide with scName in json encoding - required for deploying arbitrary",
             demand: true
+        },
+        "deploy": {
+            type: 'boolean',
+            describe: 'deploy automatically',
+            demand: false,
+            default: false
+        },
+        "privkey": {
+            type: 'string',
+            describe: 'privkey to deploy with',
+            demand: false,
+            default: undefined,
         }
     }).help(false).version(false).argv;
 
-    const args = (args_.contractName) ? args_ : yargs.options({
-        'testing': {
-            demandOption: false,
-            default: false,
-            describe: 'enable testing functions in the ballot smart contract',
-            type: 'boolean'
-        },
-        'useEncryption': {
-            demandOption: true,
-            default: false,
-            describe: 'determine whether the smart contract will accept encrypted ballots or not.',
-            type: 'boolean'
-        },
-        "deploy": {
-            describe: '_automatically_ deploy the contract (no user interaction)',
-            default: false,
-            demandOption: false,
-            type: 'boolean'
-        },
-        "startTime": {
-            demandOption: false,
-            default: 0,
-            describe: "Start time in Seconds since Unix Epoch (UTC)",
-            type: 'number'
-        },
-        "endTime": {
-            demandOption: true,
-            describe: "End time in Seconds since Unix Epoch (UTC)",
-            type: 'number'
-        },
-        "unsafeSkipChecks": {
-            describe: "This skips all confirmation checks. DRAGONS BE HERE",
-            type: 'boolean'
-        },
-        "web3Provider": {
-            describe: "URI for web3 provider - HTTP only",
-            default: "https://kovan.eth.secure.vote:8545",
-            type: 'string'
-        },
-        "contractName": {
-            describe: "The contract name of an alternate contract to deploy, do not include '.sol'",
-            type: 'string'
-        },
-        "contractDir": {
-            type: 'string',
-            describe: 'contract dir to use (expects ./CONTRACT_DIR/contracts folder to exist)',
-            default: 'svLight'
-        },
-        "argsJson": {
-            type: 'string',
-            describe: "Args to provide with contractName in json encoding - required for deploying arbitrary"
-        },
-        "specHash": {
-            describe: "The Keccak256 (eth-sha3) hash of the ballot spec",
-            type: 'string',
-            demandOption: true,
-        }
-    }).version(false).argv;
-
-    if (!args.contractName && !ethHashCheck(args.specHash)) {
-        log("Error:".bgRed.white + " Ballot Encryption Pubkey is not 32 bytes formatted in Ethereum Hex (should be 66 characters long total)")
+    if (!args.scName) {
+        log("Error:".bgRed.white + " Need to provide scName")
         process.exit(1);
     }
 
-    const contractName = args.contractName || "LittleBallotBox";
-    const [abi, bin] = loadDetails(contractName, args.contractDir);
+    const scName = args.scName;
+    const [abi, bin] = loadDetails(scName, args.scSrc);
 
-    web3.setProvider(new Web3.providers.HttpProvider(args.web3Provider));
-    const coinbase = await web3.eth.getCoinbase();
+    web3.setProvider(new Web3.providers.HttpProvider(args.web3));
+
+    let coinbase, deployAcct;
+    if (args.privkey) {
+        deployAcct = await web3.eth.accounts.privateKeyToAccount(args.privkey);
+        coinbase = deployAcct.address;
+    } else {
+        coinbase = await web3.eth.getCoinbase();
+    }
+
 
     log("\n\nSummary of Deployment:\n".cyan.bold)
-
-    if (!args.contractName) {
-        const startTime = new Date(args.startTime * 1000);
-        const endTime = new Date(args.endTime * 1000);
-        log("Start Time: " + startTime.toString().yellow, 2);
-        log("End Time: " + endTime.toString().yellow, 2);
-        log("Use Encryption: " + args.useEncryption.toString().yellow, 2);
-        log("Ballot Specification Hash:", 2)
-        log(args.specHash.yellow, 4)
-    }
     log("Sending from: " + coinbase.yellow, 2);
     log("\nBe sure to " + "double and triple check".magenta + " these before you go live!\n")
 
@@ -148,46 +100,55 @@ const main = async () => {
     // MEAT OF SCRIPT IS HERE
 
     const deployF = async () => {
-        const contract = new web3.eth.Contract(abi);
+        const sendParams = {data: "0x" + bin, from: coinbase};
+
+        const contract = new web3.eth.Contract(abi, sendParams);
 
         // set the contract deployment arguments
-        const contractArgs = args.contractName ? JSON.parse(args.argsJson) : [args.startTime, args.endTime, args.useEncryption, args.testing, args.specHash];
-
-        const sendParams = {data: "0x" + bin, from: coinbase};
+        const contractArgs = JSON.parse(args.argsJson);
 
         // organise our arguments for getting final bytecode
         const deployObj = contract.deploy({data: sendParams.data, arguments: contractArgs});
         const estGas = await deployObj.estimateGas();
         const binaryData = deployObj.encodeABI();
 
-        const compiledSendParams = R.merge(sendParams, {data: binaryData});
+        const compiledSendParams = R.merge(sendParams, {data: binaryData, gas: estGas * 1.2 | 0});
 
-        if (args.deploy) {
+        if (args.deploy && args.privkey) {
             log("About to deploy...")
             log("NOTE:".yellow + " The cli will become unresponsive until the transaction confirms. Please be patient. \n\n")
             log("\nContract Deploying!\n".green);
 
-            const deployCallback = (err, deployed) => {
-                if (err) {
-                    log("WARNING:".red + " Ran into an error while deploying contract:")
-                    log(err);
-                    log("\nStringified error: " + JSON.stringify(err));
-                    process.exit(1);
-                } else {
-                    log("Tx Hash: " + deployed.transactionHash.green);
-                    if (deployed.address) {
-                        log("Contract Addr: " + deployed.address.green + "\n\n");
-                        log("          >>> Job Done - Exiting <<<          ".bgGreen.black)
-                        process.exit(0);
-                    } else {
-                        log("Awaiting a confirmation...\n".cyan);
-                    }
-                }
-            };
+
+            // const deployCallback = (err, deployed) => {
+            //     if (err) {
+            //         log("WARNING:".red + " Ran into an error while deploying contract:")
+            //         log(err);
+            //         log("\nStringified error: " + JSON.stringify(err));
+            //         process.exit(1);
+            //     } else {
+            //         log("Tx Hash: " + deployed.transactionHash.green);
+            //         if (deployed.address) {
+            //             log("Contract Addr: " + deployed.address.green + "\n\n");
+            //             log("          >>> Job Done - Exiting <<<          ".bgGreen.black)
+            //             process.exit(0);
+            //         } else {
+            //             log("Awaiting a confirmation...\n".cyan);
+            //         }
+            //     }
+            // };
 
             // organise our final arguments and deploy!
-            const deploymentArgs = R.concat(contractArgs, [sendParams, deployCallback])
-            const r = contract.new(...deploymentArgs)
+            const signed = await deployAcct.signTransaction(compiledSendParams);
+            const r = web3.eth.sendSignedTransaction(signed.rawTransaction);
+            r.on("transactionHash", hash => {
+                console.log("Got Tx Hash".green, hash.yellow);
+            })
+            r.on("receipt", receipt => {
+                console.log("Got Tx Receipt!".green);
+                console.log("Contract Addr:".green, receipt.contractAddress.yellow);
+            })
+            return r;
         } else {
             log("Contract to deploy:\n".green.bold);
             log(JSON.stringify(compiledSendParams, null, 2))
@@ -198,19 +159,27 @@ const main = async () => {
     }
 
     if (!args.unsafeSkipChecks) {
-        correctDetails.run()
-            .then(isCorrect => {
+        return correctDetails.run()
+            .then(async isCorrect => {
                 if (!isCorrect) {
                     log("Exiting: details not correct.")
                     process.exit(0);
                 } else {
-                    return deployF();
+                    await deployF();
+                    return
                 }
             })
     } else {
-        return deployF();
+        return await deployF();
     }
 }
 
 
-main();
+main()
+    .then(() => {
+        console.log("Main returned.");
+    })
+    .catch(err => {
+        console.error("Fatal Error!".red.bold);
+        console.error(err);
+    });
