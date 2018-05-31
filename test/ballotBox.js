de019f88beafa4853baf018bb1e50f10dd93c1ee
Max Kaye <m@xk.io>
2018-05-31 17:09:03 +1000

    Add BBFarmProxy back in b/c tests rely on it

--- test/ballotBox.js
+++ test/ballotBox.js
@@ -5,7 +5,6 @@ var SvIndex = artifacts.require("./SVLightIndex");
 var SvPayments = artifacts.require("./SVPayments");
 var BallotAux = artifacts.require("./BallotAux");
 var BBFarm = artifacts.require("./BBFarm")
-var BBFarmPx = artifacts.require("./BBFarmProxy")
 var BBFarmAux = artifacts.require("./BBFarmAux")
 var TestHelper = artifacts.require("./TestHelper")
 
