d4de0c93ed2b0eb1d8b6e91974518ec15af8227d
Max Kaye <m@xk.io>
2018-06-20 12:34:36 +1000

    fix accessor method for ballotLog to match correct function sig

--- contracts/community_auction_simple.v.py
+++ contracts/community_auction_simple.v.py
@@ -3,11 +3,11 @@ owner: public(address)
 
 commBallotPriceWei: public(uint256)
 
-ballotLog: public({
+_ballotLog: {
     democHash: bytes32,
     ts: timestamp
-}[int128][address])
-ballotLog_n: int128[address]
+}[uint256][address]
+ballotLog_n: uint256[address]
 
 upgrades: address[address]
 
@@ -33,7 +33,7 @@ def getNextPrice(_: bytes32) -> uint256:
 @public
 def noteBallotDeployed(d: bytes32):
     assert not not msg.sender
-    self.ballotLog[msg.sender][self.ballotLog_n[msg.sender]] = {democHash: d, ts: block.timestamp}
+    self._ballotLog[msg.sender][self.ballotLog_n[msg.sender]] = {democHash: d, ts: block.timestamp}
     self.ballotLog_n[msg.sender] += 1
 
 @public
@@ -43,8 +43,16 @@ def upgradeMe(newSC: address):
 
 @public
 @constant
-def getBallotLogN(a: address) -> int128:
+def getBallotLogN(a: address) -> uint256:
     return self.ballotLog_n[a]
+
+@public
+@constant
+def ballotLog(a: address, i: uint256) -> bytes32[2]:
+    democHash: bytes32 = self._ballotLog[a][i].democHash
+    ts: bytes32 = convert(self._ballotLog[a][i].ts, 'bytes32')
+    return [democHash, ts]
+
 
 @public
 def setPriceWei(newPrice: uint256):
